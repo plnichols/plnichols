7ac3ad614cb1b5318c688eff9066ae62cc8dec3f
Pierre-Luc Nichols <pierrelucnichols@gmail.com>
2015-06-25 01:19:29 +0100

    Add menu toggle functionality

---
+++ app/_js/main.js
@@ -0,0 +1,42 @@
+/*
+
+Javascript
+
+*/
+
+
+// app module
+var app = {
+
+  toggleClass: function(el, className) {
+    if (el.classList) {
+      el.classList.toggle(className);
+    } else {
+      var classes = el.className.split(' ');
+      var existingIndex = classes.indexOf(className);
+
+      if (existingIndex >= 0) {
+        classes.splice(existingIndex, 1);
+      } else {
+        classes.push(className);
+      }
+
+      el.className = classes.join(' ');
+    }
+  },
+  
+  header: function() {
+    var header = document.querySelectorAll('.header')[0];
+    var hamburger = document.querySelectorAll('.hamburger')[0];
+
+    hamburger.addEventListener('click', function(e){
+      app.toggleClass(header, 'header--active');
+      app.toggleClass(hamburger, 'hamburger--active');
+    }, false);
+  }
+}
+
+
+// init
+app.header();
+
