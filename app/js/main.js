ef8dcc68d278b1e57a9b345889ded15d344d1991
Pierre-Luc Nichols <pierrelucnichols@gmail.com>
2015-07-06 23:23:52 -0400

    Toggle header on menu links click

--- app/js/main.js
+++ app/js/main.js
@@ -5,36 +5,80 @@ Javascript
 */
 
 
-// app module
-var app = {
+var app = (function() {
 
-  toggleClass: function(el, className) {
-    if (el.classList) {
-      el.classList.toggle(className);
-    } else {
-      var classes = el.className.split(' ');
-      var existingIndex = classes.indexOf(className);
+  // private
+  // insert private vars and functions here
+  // ...
+  // ...
 
-      if (existingIndex >= 0) {
-        classes.splice(existingIndex, 1);
+
+  // public
+  return {
+    toggleClass: function(el, className) {
+      if (el.classList) {
+        el.classList.toggle(className);
       } else {
-        classes.push(className);
+        var classes = el.className.split(' ');
+        var existingIndex = classes.indexOf(className);
+
+        if (existingIndex >= 0) {
+          classes.splice(existingIndex, 1);
+        } else {
+          classes.push(className);
+        }
+
+        el.className = classes.join(' ');
+      }
+    },
+
+
+    removeClass: function(el, className) {
+      if (el.classList) {
+        el.classList.remove(className);
+      }
+      else {
+        el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+      }
+    },
+
+
+    addClass: function(el, className) {
+      if (el.classList) {
+        el.classList.add(className);
+      }
+      else {
+        el.className += ' ' + className;
+      }
+    },
+
+
+    header: function() {
+
+      // private
+      var hamburger = document.querySelector('.hamburger');
+      var header = document.querySelector('.header');
+
+      hamburger.addEventListener('click', function(e) {
+        toggleHeader();
+      }, false);
+
+      header.addEventListener('click', function(e) {
+        if (e.target !== e.currentTarget) {
+          toggleHeader();
+        }
+      }, false);
+
+      function toggleHeader() {
+        app.toggleClass(header, 'header--active');
+        app.toggleClass(hamburger, 'hamburger--active');
       }
 
-      el.className = classes.join(' ');
+      // public
+      // ...
     }
-  },
-  
-  header: function() {
-    var header = document.querySelectorAll('.header')[0];
-    var hamburger = document.querySelectorAll('.hamburger')[0];
-
-    hamburger.addEventListener('click', function(e){
-      app.toggleClass(header, 'header--active');
-      app.toggleClass(hamburger, 'hamburger--active');
-    }, false);
   }
-};
+})();
 
 
 // init
