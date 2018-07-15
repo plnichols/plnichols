20e2b1fd6dc90c1580079a227a228b99fceed4c5
Pierre Luc Nichols <pierrelucnichols@gmail.com>
2018-07-15 02:18:42 +0100

    Add Divider component

--- src/components/Divider/Divider.styles.js
+++ src/components/Divider/Divider.styles.js
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
 import { color } from 'theme';
 
+const marginObj = {
+  tiny: '10px',
+  small: '20px',
+  medium: '40px',
+  large: '80px',
+  none: '0'
+};
+
 const DividerStyled = styled.hr`
+  margin: ${props => (props.size ? marginObj[props.size] : '30px')} 0;
   height: 0;
   border: 0;
   border-bottom: 1px solid ${color.lightGrey};
