e9d71fe00a558a6fa17d9411ad403105205cb4f4
Pierre Luc Nichols <pierrelucnichols@gmail.com>
2018-07-18 01:13:08 +0100

    Add Polished tool for Sass utilities

--- src/components/Heading/Heading.styles.js
+++ src/components/Heading/Heading.styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const BaseHeading = styled.span`
   margin: 0 0 2rem;
-  line-height: 1;
+  line-height: 1.25;
   font-weight: 300;
   letter-spacing: -1px;
 `;
@@ -13,16 +13,16 @@ const H1Styled = BaseHeading.withComponent('h1').extend`
 
 const H2Styled = BaseHeading.withComponent('h2').extend`
   font-size: ${props => props.size || '4rem'};
+  margin-bottom: 1rem;
 `;
 
 const H3Styled = BaseHeading.withComponent('h3').extend`
   font-size: ${props => props.size || '3rem'};
-  margin-bottom: 1rem;
+  margin-bottom: 0.5rem;
 `;
 
 const H4Styled = BaseHeading.withComponent('h4').extend`
   font-size: ${props => props.size || '2.4rem'};
-  margin-bottom: 1rem;
 `;
 
 const H5Styled = BaseHeading.withComponent('h5').extend`
