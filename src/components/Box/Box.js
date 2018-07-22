73685c0c2b24dd6ac0d7adab30a66275b90ea97a
Pierre Luc Nichols <pierrelucnichols@gmail.com>
2018-07-22 21:24:09 +0100

    Add Contact component and refactor WithSpacing HoC

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,27 +1,11 @@
 import React from 'react';
+import { WithSpacing } from 'hoc';
 import { BoxStyled } from './Box.styles';
 
-const Box = ({ m, mx, my, mt, mr, mb, ml, p, px, py, pt, pr, pb, pl, children }) => {
-  return (
-    <BoxStyled
-      m={m}
-      mx={mx}
-      my={my}
-      mt={mt}
-      mr={mr}
-      mb={mb}
-      ml={ml}
-      p={p}
-      px={px}
-      py={py}
-      pt={pt}
-      pr={pr}
-      pb={pb}
-      pl={pl}
-    >
-      {children}
-    </BoxStyled>
-  );
+const BoxComponent = ({ children, ...rest }) => {
+  return <BoxStyled {...rest}>{children}</BoxStyled>;
 };
+
+const Box = WithSpacing(BoxComponent);
 
 export { Box };
--- src/hoc/index.js
+++ src/hoc/index.js
@@ -1,1 +1,1 @@
-export { withSpacing } from './spacing';
+export { WithSpacing } from './spacing';
